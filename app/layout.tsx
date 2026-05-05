5e85ba7b6c7aa9a21bc79030cb068ff19b1179dd
Erick Hernández Torres <satcoresolutions@gmail.com>
2026-05-04 19:03:01 -0500

    Version 1.2.2
    Esta version incluye las redes sociales en footer, incluye el botón de whatsapp y puede

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WhatsAppButton from "@/src/components/ui/WhatsAppButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -76,7 +77,14 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        {children}
+
+        {/* 👇 AQUÍ va el botón flotante */}
+        <WhatsAppButton />
+
+      </body>
     </html>
   );
 }
